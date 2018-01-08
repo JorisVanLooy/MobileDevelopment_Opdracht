cd1c3bef1e20835a04149627c59fa92adbea3858
Joris Van Looy <s096257@ap.be>
2018-01-08 17:53:21 +0100

    Image links uit JSON data halen en deze foto's downloaden naar bitmaps.

--- app/src/main/java/com/example/joris/mobiledevelopment_opdracht/Screen3.java
+++ app/src/main/java/com/example/joris/mobiledevelopment_opdracht/Screen3.java
@@ -1,26 +1,24 @@
 package com.example.joris.mobiledevelopment_opdracht;
 
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.joris.mobiledevelopment_opdracht.GetImgurImages.AsyncResponse;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Screen3 extends AppCompatActivity  {
-    public String JSON;
+public class Screen3 extends AppCompatActivity implements AsyncResponse  {
     public String Term;
+    private GetImgurImages ImageTask;
+    public ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,53 +27,27 @@ public class Screen3 extends AppCompatActivity  {
         //final CustomImageSearch ImageSearch = new CustomImageSearch();
         final ListView listView = (ListView)findViewById(R.id.ListView1);
         final TextView textView =(TextView)findViewById(R.id.textView3);
-        final List<String> Links = new ArrayList<>();
         final Button Search = (Button)findViewById(R.id.SearchBtn);
         final EditText text = (EditText)findViewById(R.id.SearchTerm);
+        imageView = (ImageView)findViewById(R.id.imageView);
+        ImageTask = new GetImgurImages(this);
 
-        final GetImgurImages ImageSearch = new GetImgurImages(new AsyncResponse() {
-            @Override
-            public void processFinish(JSONObject output) {
-                try{
-                    JSONArray Data = output.getJSONArray("data");
-                    for(int i =0; i < Data.length();i++){
-                        JSONObject DataItem = Data.getJSONObject(i);
-
-                        if(DataItem.has("images")){
-                            JSONArray Images = DataItem.getJSONArray("images");
-                            for(int j =0; j < Images.length(); j++){
-                                JSONObject ImageItem = Images.getJSONObject(j);
-                                String ImageLink = ImageItem.getString("link");
-                                Links.add(ImageLink);
-                            }
-                        }
-                    }
-                    //textView.setText(Data.getJSONObject(0).toString());
-                    textView.setText(Links.get(0));
-                }
-                catch (JSONException e){
-                    Log.e("Error", e.getMessage(),e);
-                }
-            }
-        });
-
-
-        //listView.setAdapter(new LinkArrayAdapter(this,LinksArray));
-
-
-        Search.setOnClickListener(new View.OnClickListener() {
+        Search.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                try{
-                    Term = text.getText().toString();
-                    ImageSearch.searchTerm = Term;
-                    ImageSearch.execute();
+                Term = text.getText().toString();
+                if(ImageTask.getStatus() == AsyncTask.Status.RUNNING){
+                    Log.d("Task", "onClick: ImageTask already running");
                 }
-                catch (IllegalStateException e){
-
+                else{
+                    ImageTask.execute();
                 }
             }
         });
     }
 
+    @Override
+    public void processFinish(Bitmap[] output) {
+        imageView.setImageBitmap(output[0]);
+    }
 }
